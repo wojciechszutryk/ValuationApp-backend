e19ccd7fbc5485befee6754789110ed4c23ff546
szutryk <wojtekszutryk@gmail.com>
2021-10-19 11:41:08 +0200

    add works_get_valuationObjects route

--- routers/works.ts
+++ routers/works.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import {works_create, works_delete, works_get_all, works_get_single} from "../conrollers/works";
+import {works_create, works_delete, works_get_all, works_get_single, works_get_valuationObjects} from "../conrollers/works";
 import {checkAuth} from "../middlewares/check-auth";
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get('/', works_get_all);
 // router.get('/', checkAuth, works_get_all);
 router.post('/', works_create);
 router.get(`/:id`, works_get_single);
+router.get(`/:id/valuationObjects`, works_get_valuationObjects);
 router.delete(`/:id`, works_delete);
 
 export {router};
